91298a4fd1e4e9616bd856d9ea3c97e362cd788c
bme <eddygrove@gmail.com>
2022-11-19 02:52:20 +0100

    added matching, animation and player divs

--- js/apptilt.js
+++ js/apptilt.js
@@ -7,6 +7,14 @@
 // img borde vara oberoende av ratio på bilder, och evxvis klippa overflow till kvadrat. Vore kul om användaren kan lägga in egna bilder/bildlänkar i framtida version.
 //ge
 
+let playerCount = 2;
+let playerCurrent = 1;
+let playerCurrentPoints = 0;
+let cardsChecked = 0;
+let matchClass = "";
+let darkMode = false;
+let checkboxID = 1;
+
 // skapar första spelplanen
 document.body.onload = createBoard;
 // tilldelar div game-area till variabel gameArea
@@ -14,7 +22,6 @@ const gameArea = document.querySelector(".table");
 const myDeck = document.createElement("div");
 myDeck.classList.add("deck");
 gameArea.appendChild(myDeck);
-console.log("skapat Dynamical Deck°");
 // array av alla bildnamn i img-mapp. två av varje.
 let unshuffled = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10];
 // blanda dessa slumpmässigt
@@ -23,70 +30,85 @@ let shuffled = unshuffled
   .sort((a, b) => a.sort - b.sort)
   .map(({ value }) => value);
 
-let untilted = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-let tilted = untilted
-  .map((value) => ({ value, sort: Math.random() }))
-  .sort((a, b) => a.sort - b.sort)
-  .map(({ value }) => value);
+// let untilted = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// let tilted = untilted
+//   .map((value) => ({ value, sort: Math.random() }))
+//   .sort((a, b) => a.sort - b.sort)
+//   .map(({ value }) => value);
 
 // befolka spelplanen med slumpmässigt blandade kort
 function createBoard() {
   for (let deal of shuffled) {
-    let myCard = document.createElement("input");
-    myCard.type = "checkbox";
-    myCard.id = "toggle-" + deal;
-    // ge checkboxen klass för att flytta den ur sidan
-    // myCard.classList.add("checkbox_hidden");
-    // let container = document.createElement("label");
-    // container.classList.add("container");
-    // container.htmlFor = "toggle-" + deal;
-    let tilt = document.createElement("div");
-    tilt.classList.add("tilt");
-    let rotation = Math.floor(Math.random() * (20 - -19 + 1) + -19);
+    let myCheckbox = document.createElement("input");
+    myCheckbox.type = "checkbox";
+    myCheckbox.classList.add("pair-" + deal);
+
+    myCheckbox.id = checkboxID;
+    checkboxID = checkboxID + 1;
+    let card = document.createElement("div");
+    card.classList.add("card");
+    card.classList.add("pair-" + deal);
+    let rotation = Math.floor(Math.random() * (20 - -20 + 1) + -20);
     // let horMargin = Math.floor(
     //   Math.random() * (20 - 0 + rotation / 3) + rotation / 3
     // );
     let horMargin = Math.floor(Math.random() * (20 - 0 + 1) + 1);
-    let verMargin = Math.floor(Math.random() * (20 - 0 + 1) + 0);
-    tilt.style.transform = `rotate(${rotation}deg)`;
-    tilt.style.margin = `${verMargin}px ${horMargin}px`;
+    let verMargin = Math.floor(Math.random() * (10 - 0 + 1) + 1);
+    card.style.transform = `rotate(${rotation}deg)`;
+    card.style.margin = `${verMargin}px ${horMargin}px`;
     let front = document.createElement("u");
-    // front.classList.add("card", "front");
     let back = document.createElement("b");
-    // back.classList.add("card", "back");
     let newImage = document.createElement("img");
     newImage.src = "img/" + deal + ".jpg";
-    // newImage.classList.add("front-image");
-    myDeck.appendChild(tilt);
-    tilt.appendChild(myCard);
-    tilt.appendChild(front);
+    myDeck.appendChild(card);
+    card.appendChild(myCheckbox);
+    card.appendChild(front);
     front.appendChild(newImage);
-    tilt.appendChild(back);
+    card.appendChild(back);
   }
 }
 
-{
-  /* <input type="checkbox" id="toggle-1">
-<label class="container" for="toggle-1">
-  <div class="card front"></div>
-  <div class="card back">i am back</div>
-</label> */
-}
+// let btn = document.getElementById("btn");
+// btn.addEventListener("click", (event) => {
+//   let cardMatch = document.querySelectorAll(".pair-5");
+//   for (let aCard of cardMatch) {
+//     aCard.classList.toggle("collect-p1");
 
-// befolka spelplanen med slumpmässigt blandade kort
-{
-  /* function createBoard() {
-  for (let deal of shuffled) {
-    let card = document.createElement("div");
-    card.classList.add("card");
-    let innerCard = document.createElement("div");
-    innerCard.classList.add("card_inner");
-    let newImage = document.createElement("img");
-    newImage.src = "img/" + deal + ".jpeg";
-    newImage.classList.add("front-image");
-    gameArea.appendChild(card);
-    card.appendChild(innerCard);
-    innerCard.appendChild(newImage);
+//     aCard.style.transform = `translate(-70px, -100px)`;
+//     aCard.style.transition = `transform 1500ms ease-in`;
+//   }
+// });
+
+// Target all clicks on any element
+document.addEventListener("click", (e) => {
+  // Get element class(es)
+  let pickedClass = e.target.className;
+  //   ***** additional class since js doesn't honour unchecked input state:
+  e.target.classList.toggle("checked");
+  console.log("listener picked Class: ", pickedClass);
+  if (pickedClass === matchClass) {
+    console.log("Card MATCH!: ", matchClass);
+    matchClass = "";
+    console.log("A match done, emptying matchClass:", matchClass);
+    const matchPair = document.querySelectorAll("." + pickedClass);
+    console.log("matchPair ", matchPair);
+    matchPair.forEach((match) => {
+      match.style.transition = "transform 500ms ease-in 1.2s";
+      //   match.style.animationDelay = "1s";
+      match.style.transform = "translate(-300px, -1000px)";
+      console.log("match ", match);
+    });
+  } else {
+    matchClass = pickedClass;
+    console.log("No match yet, setting matchClass:", matchClass);
   }
-} */
-}
+});
+
+// If element has class(es)
+//   if (matchClass !== "") {
+//     console.log("this element has the class: ", matchClass);
+//   }
+//   // If element has no classes
+//   else {
+//     console.log("An element without a class was clicked");
+//   }
